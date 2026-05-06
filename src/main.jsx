e4892173d0e0be4610afbd0bba758404bb3c6460
CHENO1983 <chenn1983@gmail.com>
2026-05-06 23:11:06 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import OpaMarketing from './OpaMarketing.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <OpaMarketing />
+  </React.StrictMode>
+)
